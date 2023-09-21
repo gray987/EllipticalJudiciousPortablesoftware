9ab2f5a8b8de854ff11036a7931d1b9a62e45f9a
grayweaver <25957510-grayweaver@users.noreply.replit.com>
2023-09-21 17:25:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+response = input()
+counter = 0
+while response != "quit":
+    response = input()
+    for letter in response:
+        if letter == "s" or letter == "s":
+            counter = counter + 1
+    print(counter)    
+    counter = 0    
+print("goodbye")  
