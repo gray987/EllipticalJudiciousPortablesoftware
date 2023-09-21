4bed8f228361dfa86f5e9faa702e45b0c7a3ffea
gray987 <42500195+gray987@users.noreply.github.com>
2023-09-21 17:37:36 +0000

    made it worse

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 response = input()
 counter = 0
 while response != "quit":
-    response = input()
     for letter in response:
         if letter == "s" or letter == "s":
-            counter = counter + 1
+            counter = counter + 5
     print(counter)    
     counter = 0    
+    response = input()
+
 print("goodbye")  
